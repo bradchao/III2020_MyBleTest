90caa5f0738546b9eb4be9ba59a62b957f77d9df
bradchao <bk400288>
2020-08-01 16:04:35 +0800

    080102

--- app/src/main/java/tw/brad/apps/mybletest/MainActivity.java
+++ app/src/main/java/tw/brad/apps/mybletest/MainActivity.java
@@ -7,6 +7,13 @@ import androidx.core.content.ContextCompat;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import com.inuker.bluetooth.library.BluetoothClient;
+import com.inuker.bluetooth.library.search.SearchRequest;
+import com.inuker.bluetooth.library.search.SearchResult;
+import com.inuker.bluetooth.library.search.response.SearchResponse;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -37,8 +44,39 @@ public class MainActivity extends AppCompatActivity {
             }
         }
     }
-
+    private BluetoothClient mClient;
     private void init(){
+        mClient = new BluetoothClient(this);
+    }
+    public void scanDevices(View view) {
+        SearchRequest request = new SearchRequest.Builder()
+                .searchBluetoothLeDevice(3000, 3)   // 先扫BLE设备3次，每次3s
+                .searchBluetoothClassicDevice(5000) // 再扫经典蓝牙5s
+                .searchBluetoothLeDevice(2000)      // 再扫BLE设备2s
+                .build();
+        mClient.search(request, new SearchResponse() {
+            @Override
+            public void onSearchStarted() {
+                Log.v("bradlog", "scan...");
+            }
+
+            @Override
+            public void onDeviceFounded(SearchResult device) {
+                String name = device.getName();
+                String mac = device.getAddress();
+                Log.v("bradlog", name + " => " + mac);
+            }
+
+            @Override
+            public void onSearchStopped() {
+                Log.v("bradlog", "scan stop");
+            }
+
+            @Override
+            public void onSearchCanceled() {
+                Log.v("bradlog", "scan cancel");
+            }
+        });
 
     }
 }
